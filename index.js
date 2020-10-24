dc99b20db2fd79c6b15fa1f4eadcd4cd4af3a00b
YazzCoronado <yazz_coronado01@hotmail.com>
2020-10-24 11:38:29 -0600

    Agregadas Figuras

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Ejercicio con Node y Express");
+
+console.log("Ejercicio con Node y Express");
+
+// index.js
+const { triangle, square, rhombus } = require('print-shape')
+ 
+triangle(15)
+square(10)
+rhombus(20)
